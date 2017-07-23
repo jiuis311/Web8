9fc5e8b81100cb6f6e2eb67f8ceb25c4b464837c
jiuis <ltlinh311@gmail.com>
2017-07-23 23:38:36 +0700

    Gme ban ruoi - 2 players (reverted from commit 8789d2d1ef2daac8dce507e79c5f8638f1e3ae13)

--- js/main.js
+++ js/main.js
@@ -18,6 +18,9 @@ window.onload = function(){
   );
 }
 
+//size
+var playerx, playery;
+
 // preparations before game starts
 var preload = function(){
   Nakama.game.scale.minWidth = Nakama.configs.GAME_WIDTH/2;
@@ -41,17 +44,23 @@ var create = function(){
   Nakama.background = Nakama.game.add.sprite(0,-960,'background');
   Nakama.player = Nakama.game.add.sprite(400,700,'assets','Spaceship1-Player.png');
   Nakama.partner = Nakama.game.add.sprite(200,700,'assets','Spaceship1-Partner.png');
+  playerx = Nakama.player.width;
+  playery = Nakama.player.height;
 }
 
 // update game state each frame
 var update = function(){
+  Nakama.player.width = playerx;
+  Nakama.player.height = playery;
   Nakama.background.position.y += 5;
   if (Nakama.background.position.y > 0) Nakama.background.position.y = -960;
   if (Nakama.keyboard.isDown(Phaser.Keyboard.LEFT) && Nakama.player.position.x > 0) {
     Nakama.player.position.x -= 10;
+    Nakama.player.width *= 0.75;
   }
   if (Nakama.keyboard.isDown(Phaser.Keyboard.RIGHT) && Nakama.player.position.x < Nakama.configs.GAME_WIDTH-Nakama.player.width) {
     Nakama.player.position.x += 10;
+    Nakama.player.width *= 0.75;
   }
   if (Nakama.keyboard.isDown(Phaser.Keyboard.UP) && Nakama.player.position.y > 0) {
     Nakama.player.position.y -= 10;
